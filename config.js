0ff180120cfe70d16cdbaa4103aa2382f3a02eb5
iSergeki <mizitools@mail.ru>
2025-06-19 03:59:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
     db: {        
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'fmf'
+        port: "3306",
+        user: "gen_user",
+        host: "188.225.27.185",
+        database: "default_db",
+        password: "qwer1234"
     },
     port: 3000
 };
