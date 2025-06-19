9ce23162b062d3d6b1a1351d6c8a763cb6badd36
iSergeki <mizitools@mail.ru>
2025-06-19 17:55:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     db: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT || 3306,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DBNAME,
+        port: process.env.MYSQL_PORT || 3306,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
